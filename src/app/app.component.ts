e2ebe43c5e8ff0b24ec8f9e4ec7e732d87a9c380
Vishwanath Hegde <vishwahegde27@gmail.com>
2025-01-14 01:03:01 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'signal-counter';
+
+  count = signal(0);
+  doubleCount = computed(() => this.count() * 2)
+
+  constructor() {
+    effect(() => {
+      console.log(`current value of count is ${this.count()}`);
+    })
+  }
+
+  increment() {
+    this.count.update((c: number) => c + 1)
+  }
+
+  decrement() {
+    this.count.update((c) => c - 1)
+  }
+
+  reset() {
+    this.count.set(0);
+  }
 }
