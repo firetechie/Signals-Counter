ccd386d811e18ee75ef8594d2910f016b271d5e3
Vishwanath Hegde <vishwahegde27@gmail.com>
2025-01-14 02:13:27 +0530

    Zoneless ChangeDetection Counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, effect, signal } from '@angular/core';
+import { Component, computed, effect, signal, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,27 +6,62 @@ import { Component, computed, effect, signal } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'signal-counter';
 
-  count = signal(0);
-  doubleCount = computed(() => this.count() * 2)
+  count1 = signal(0);
+  doubleCount1 = computed(() => this.count1() * 2)
+  count2 = signal(0);
+  doubleCount2 = computed(() => this.count2() * 2)
+  count3 = signal(0);
+  doubleCount3 = computed(() => this.count3() * 2)
+  intervalId !: any;
 
   constructor() {
     effect(() => {
-      console.log(`current value of count is ${this.count()}`);
+      console.log(`current value of count is Counter 1 : ${this.count1()}, - Counter 2 : ${this.count2()}, Counter 3 : ${this.count3()}`)
     })
   }
 
-  increment() {
-    this.count.update((c: number) => c + 1)
+  // Basic Counter
+  increment(): void {
+    this.count1.update((c: number) => c + 1)
   }
 
-  decrement() {
-    this.count.update((c) => c - 1)
+  decrement(): void {
+    this.count1.update((c) => c - 1)
   }
 
-  reset() {
-    this.count.set(0);
+  reset(): void {
+    this.count1.set(0);
+  }
+
+  // setInterval()
+  start(): void {
+    this.intervalId = setInterval(() => {
+      this.count2.update((c) => c +
+        1)
+    }, 500);
+  }
+
+  stop(): void {
+    clearInterval(this.intervalId)
+  }
+
+  clear(): void {
+    this.count2.set(0);
+  }
+
+  // setTimeout()
+  run(): void {
+    setTimeout(() => {
+      this.count3.update((c) => c + 1)
+    }, 500);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId)
+    }
   }
 }
