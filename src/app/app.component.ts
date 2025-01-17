abfaff436b1735829379c5a8ccd32b9009d4e961
Vishwanath Hegde <vishwahegde27@gmail.com>
2025-01-18 01:18:46 +0530

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, effect, signal, OnDestroy, linkedSignal } from '@angular/core';
+import { Component, computed, effect, signal, OnDestroy, linkedSignal, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +6,20 @@ import { Component, computed, effect, signal, OnDestroy, linkedSignal } from '@a
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'signal-counter';
+
+  //Notes:
+  // Signal() is wrapper around the value and sends the latest value to the consumer by detecting it once the property value changes, it handles the reactivity.
+  // 1=> before angular is introducing signals() we were dependent on angular change detection strategy/cycles for logging it using ngDoCheck() hook.
+  // If we make the application as zoneless by removing Zone.js from polyfills object in Angular.json file and replace provideZoneChangeDetection() with provideExperimentalZonelessChangeDetection() app.config.ts angular doesn't have enoguh capacity to bind the changes in template so that's why Signals() came into the picture.
+  // 2=> ChangeDetecionStrategy disadvantage is it will execute even if we trigger the event but not chnging the value.
+  // 3=> since angular depends on zone.js it increases the bundle size and it will inform angular which component/which part of the component has been changed even if small changes happened in one component it will renders all the component
+  // zoneless will hdanle change detection by default for callbacks()/manual event trigger, observable async pipe, property received as input() without even usinng signals()
+  // 4=> CMD to clean unused imports in standalone components
+  // ng g @angular/core:cleanup-unused-imports 
+
+  typedText = signal('type something');
 
   count1 = signal(0);
   doubleCount1 = computed(() => this.count1() * 2)
@@ -17,10 +29,28 @@ export class AppComponent implements OnDestroy {
   doubleCount3 = linkedSignal(() => this.count3() * 2)
   intervalId !: any;
 
+  color = signal<string[]>(['Red', 'Blue'])
+  newColorRender = signal<string>('')
+
   constructor() {
     effect(() => {
       console.log(`current value of count is Counter 1 : ${this.count1()}, - Counter 2 : ${this.count2()}, Counter 3 : ${this.count3()}`)
+
+      //colors
+      this.newColorRender.set(`with newly added colors is displaying above i.e. ${this.color()}`)
     })
+  }
+
+  ngOnInit(): void {
+    // Colors
+    setTimeout(() => {
+      this.color.update((colors: any) => [...colors, 'Green'])
+    }, 2000);
+  }
+
+  onType(event: Event): void {
+    const text = (event.target as HTMLInputElement).value
+    this.typedText.set(text);
   }
 
   // Basic Counter
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    // till v16 if we enable the SSR to the application; without hydration, it leads to UI flickering because for example in our app we have multiple posts need to display so wwhen user will request to server, server sends the response back to the browser with lists of posts, then browser will parse and disaply in the UI then destroy the server side DOM and replace with client side one.
+    // so provideClientHydration will tries to match the existing UI structure instead of destryoing and rerendering the DOM
+    provideClientHydration(),
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes)]
 };
